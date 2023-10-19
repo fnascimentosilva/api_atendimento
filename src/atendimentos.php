d1048ff478d4c65b0747c9e3a08f1a6f1fe62569
fnascimentosilva <fabricionsilva26@gmail.com>
2023-10-19 11:19:40 +0100

    modificando a estrutura da api

--- src/atendimentos.php
+++ src/atendimentos.php
@@ -26,10 +26,27 @@ if ($method === 'POST') {
     $fila = json_decode(file_get_contents('filaAtendimento.txt'));
 
     //pega o primeiro item do array
-    //excluir a pessoa do array de fila
+     //excluir a pessoa do array de fila
+    $primeiroCliente = array_shift($fila);
+
+    file_put_contents('filaAtendimento.txt', json_encode($fila));
 
     //identificar qual o guiche de atendimento
     //fazer um push do item retirado do array de fila
+
+    if($guiche === 1){
+        $listaGuiche1 = json_decode(file_get_contents('guiche1.txt'));
+        array_push($listaGuiche1, $primeiroCliente);
+        file_put_contents('guiche1.txt', json_encode($primeiroCliente));
+    }else if($guiche === 2){
+        $listaGuiche2 = json_decode(file_get_contents('guiche2.txt'));
+        array_push($listaGuiche2, $primeiroCliente);
+        file_put_contents('guiche2.txt', json_encode($primeiroCliente));
+    }else if($guiche === 3){
+        $listaGuiche3 = json_decode(file_get_contents('guiche3.txt'));
+        array_push($listaGuiche3, $primeiroCliente);
+        file_put_contents('guiche3.txt', json_encode($primeiroCliente));
+    }
 
     if($type === 1){
     array_push($filaAtendimento, ['nome' => $nome, 'cpf' => $cpf]);
--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@ if ($method === 'POST') {
 
     $nome = filter_var($body->nome, FILTER_SANITIZE_SPECIAL_CHARS);
     $cpf = filter_var($body->cpf, FILTER_SANITIZE_SPECIAL_CHARS);
-    $type = filter_var($body -> type, FILTER_VALIDATE_INT);
+    $type = filter_var($body->type, FILTER_VALIDATE_INT);
 
     if (!$nome || !$cpf || !$type) {
         echo json_encode(['error' => 'faltam informacoes para o atendimento']);
@@ -28,12 +28,12 @@ if ($method === 'POST') {
 
     $filaAtendimento = json_decode(file_get_contents('filaAtendimento.txt'));
 
-    if($type === 1){
-    array_push($filaAtendimento, ['nome' => $nome, 'cpf' => $cpf]);
-}else{
-    //coloca os dados inicio do array
-    array_unshift($filaAtendimento, ['nome' => $nome, 'cpf' => $cpf]);
-}
+    if ($type === 1) {
+        array_push($filaAtendimento, ['nome' => $nome, 'cpf' => $cpf]);
+    } else {
+        //coloca os dados inicio do array
+        array_unshift($filaAtendimento, ['nome' => $nome, 'cpf' => $cpf]);
+    }
     file_put_contents('filaAtendimento.txt', json_encode($filaAtendimento));
 
     http_response_code(201);
